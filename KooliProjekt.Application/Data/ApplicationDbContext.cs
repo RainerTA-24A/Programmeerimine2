0a4b5e7aab8a1bf864d8c1b49acef35e67253003
RainerTA-24A <rainer.tahker@tptlive.ee>
2025-10-23 20:58:43 +0300

    GL
    holy damn

--- KooliProjekt.Application/Data/ApplicationDbContext.cs
+++ KooliProjekt.Application/Data/ApplicationDbContext.cs
@@ -13,10 +13,36 @@ namespace KooliProjekt.Application.Data
         {
         }
 
-        //  klassid
         public DbSet<Toode> Tooted { get; set; }
         public DbSet<Klient> Kliendid { get; set; }
         public DbSet<Arve> Arved { get; set; }
         public DbSet<Tellimus> Tellimused { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Lisame lihtsad konfiguratsioonid, et EF teaks suhted ja cascade käitumise.
+            modelBuilder.Entity<Klient>()
+                .HasMany(k => k.Arved)
+                .WithOne(a => a.Klient)
+                .HasForeignKey(a => a.KlientId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Arve>()
+                .HasMany(a => a.Tellimused)
+                .WithOne(t => t.Arve)
+                .HasForeignKey(t => t.ArveId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Toode>()
+                .HasKey(t => t.Id);
+
+            modelBuilder.Entity<Tellimus>()
+                .HasOne(t => t.Toode)
+                .WithMany()
+                .HasForeignKey(t => t.ToodeId)
+                .OnDelete(DeleteBehavior.Restrict); // ära kustuta toodet kui real on viiteid
+        }
     }
 }
--- KooliProjekt.Application/Data/Klient.cs
+++ KooliProjekt.Application/Data/Klient.cs
@@ -11,10 +11,10 @@ namespace KooliProjekt.Application.Data
         public int Id { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
-        public string Email { get; set;}
+        public string Email { get; set; }
         public string Phone { get; set; }
         public decimal Discount { get; set; }
 
-        public List<Arve> Arved {  get; set; }
+        public List<Arve> Arved { get; set; } = new List<Arve>();
     }
 }
