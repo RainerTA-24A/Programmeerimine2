5f20ea326e9bc63b922d231d565b2e370a9da887
RainerT <rainer.tahker@tptlive.ee>
2026-04-19 13:16:52 +0300

    16/04/2026
    WPF dialoogide kuvamine
    
    new, save, delete töötab

--- KooliProjekt.WpfApplication/MainWindowViewModel.cs
+++ KooliProjekt.WpfApplication/MainWindowViewModel.cs
@@ -1,23 +1,77 @@
 ﻿using System.Collections.ObjectModel;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace KooliProjekt.WpfApplication
 {
     public class MainWindowViewModel : NotifyPropertyChangedBase
     {
         private readonly IApiClient _apiClient;
+        private readonly IDialogProvider _dialogProvider;
         private readonly ObservableCollection<Toode> _data;
         private Toode _selectedItem;
 
-        public MainWindowViewModel() : this(new ApiClient())
+        public ICommand AddNewCommand { get; private set; }
+        public ICommand SaveCommand { get; private set; }
+        public ICommand DeleteCommand { get; private set; }
+
+        public MainWindowViewModel() : this(new ApiClient(), new DialogProvider())
         {
             
         }
 
-        public MainWindowViewModel(IApiClient apiClient)
+        public MainWindowViewModel(IApiClient apiClient, IDialogProvider dialogProvider)
         {
             _data = new ObservableCollection<Toode>();
             _apiClient = apiClient;
+            _dialogProvider = dialogProvider;
+
+            AddNewCommand = new RelayCommand<Toode>(
+                toode =>
+                {
+                    SelectedItem = new Toode();
+                });
+
+            SaveCommand = new RelayCommand<Toode>(
+                async toode =>
+                {
+                    var result = await _apiClient.SaveToode(toode);                    
+                    if(result.HasErrors)
+                    {
+                        ShowError("Cannot save data", result);
+                        return;
+                    }
+
+                    SelectedItem = null;
+                    await LoadDataAsync();
+                },
+                toode =>
+                {
+                    return SelectedItem != null;
+                });
+
+            DeleteCommand = new RelayCommand<Toode>(
+                async toode =>
+                {
+                    var canDelete = _dialogProvider.Confirm("Are you sure you want to delete this item?");  
+                    if(!canDelete)
+                    {
+                        return;
+                    }
+
+                    var result = await _apiClient.DeleteToode(toode.Id);
+                    if(result.HasErrors)
+                    {
+                        ShowError("Cannot delete data", result);
+                        return;
+                    }
+                    SelectedItem = null;
+                    await LoadDataAsync();
+                },
+                toode =>
+                {
+                    return SelectedItem != null && SelectedItem.Id != 0;
+                });
         }
 
         public async Task LoadDataAsync()
@@ -50,6 +104,43 @@ namespace KooliProjekt.WpfApplication
                 _selectedItem = value;
                 NotifyPropertyChanged();
             }
+        }
+
+        public void ShowError(string message, OperationResult result)
+        {
+            var error = message + "\r\n";
+            var apiErrors = "";
+            var propertyErrors = "";
+
+            if (result.Errors != null)
+            {
+                foreach (var apiError in result.Errors)
+                {
+                    apiErrors += apiError + "\r\n";
+                }
+            }
+
+            if (result.PropertyErrors != null)
+            {
+                foreach (var propertyError in result.PropertyErrors)
+                {
+                    propertyErrors += propertyError.Key + ": " + propertyError.Value;
+                }
+            }
+
+            if (!string.IsNullOrEmpty(apiErrors))
+            {
+                error += "\r\n" + apiErrors + "\r\n";
+            }
+
+            if (!string.IsNullOrEmpty(propertyErrors))
+            {
+                error += "\r\n" + propertyErrors;
+            }
+
+            error = error.Trim();
+
+            _dialogProvider.ShowError(error);
         }
     }
 }
