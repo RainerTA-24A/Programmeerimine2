5639bd25c350d980b55926abd8fb3030e31fe55e
RainerT <rainer.tahker@tptlive.ee>
2026-05-13 18:53:28 +0300

    07.05 Töö
    Edit ja Add new nupp töötavad nüüd

--- KooliProjekt.BlazorWasm/Api/ApiClient.cs
+++ KooliProjekt.BlazorWasm/Api/ApiClient.cs
@@ -25,6 +25,17 @@ namespace KooliProjekt.BlazorWasm
             return result;
         }
 
+        public async Task<OperationResult<Toode>> Get(int id)
+        {
+            var url = _baseUrl + "Get?id=" + id;
+            using var request = new HttpRequestMessage(HttpMethod.Get, url);
+            using var response = await _client.SendAsync(request);
+            var body = await response.Content.ReadAsStringAsync();
+
+            var result = JsonConvert.DeserializeObject<OperationResult<Toode>>(body);
+            return result;
+        }
+
         public async Task<OperationResult> Save(Toode list)
         {
             var url = _baseUrl + "Save";
